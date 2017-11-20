9cfd0e1a27ac642ecc49d87b49b73e6c8612b384
Justin van Gemen <20277@ma-web.nl>
2017-11-20 15:14:22 +0100

    d o n e

--- Assets/_Scripts/HexMesh.cs
+++ Assets/_Scripts/HexMesh.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class HexMesh : MonoBehaviour {
 	private Mesh _hexMesh;
+	private List<Vector3> _verts;
 	private List<Color> _colors;
 	private List<int> _triangles;
 
@@ -13,17 +14,20 @@ public class HexMesh : MonoBehaviour {
 		GetComponent<MeshFilter>().mesh = _hexMesh = new Mesh();
 		_meshCollider = gameObject.AddComponent<MeshCollider>();
 		_hexMesh.name = "Hex Mesh";
+		_verts = new List<Vector3>();
 		_colors = new List<Color>();
 		_triangles = new List<int>();
 	}
 
 	public void Triangulate (HexCell[] cells) {
 		_hexMesh.Clear();
+		_verts.Clear();
 		_colors.Clear();
 		_triangles.Clear();
 		for (var i = 0; i < cells.Length; i++) {
 			Triangulate(cells[i]);
 		}
+		_hexMesh.vertices = _verts.ToArray();
 		_hexMesh.colors = _colors.ToArray();
 		_hexMesh.triangles = _triangles.ToArray();
 		_hexMesh.RecalculateNormals();
@@ -43,6 +47,10 @@ public class HexMesh : MonoBehaviour {
 	}
 
 	private void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+		var vertexIndex = _verts.Count;
+		_verts.Add(v1);
+		_verts.Add(v2);
+		_verts.Add(v3);
 		_triangles.Add(vertexIndex);
 		_triangles.Add(vertexIndex + 1);
 		_triangles.Add(vertexIndex + 2);
